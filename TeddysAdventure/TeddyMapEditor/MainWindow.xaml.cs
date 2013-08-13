031b737e52f030d8af9f7591e5cfe8407810b5b1
orlando008 <orlando008@gmail.com>
2013-08-12 22:06:32 -0400

    Beginnings of map editor

---
+++ TeddysAdventure/TeddyMapEditor/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeddyMapEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int BASIC_UNIT = 32;
+        private bool _clickCaptured = false;
+        private Point _clickStart;
+        private Point _clickEnd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLevelWidth_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cnvsMap.Width = Convert.ToInt32(txtLevelWidth.Text);
+            DrawGrid();
+        }
+
+        private void txtLevelHeight_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cnvsMap.Height = Convert.ToInt32(txtLevelHeight.Text);
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            cnvsMap.Children.Clear();
+
+            for (int i = 0; i < cnvsMap.Width / BASIC_UNIT; i++)
+            {
+                Line theVerticalLine = new Line();
+                theVerticalLine.X1 = i * BASIC_UNIT;
+                theVerticalLine.X2 = i * BASIC_UNIT;
+                theVerticalLine.Y1 = 0;
+                theVerticalLine.Y2 = cnvsMap.Height;
+                theVerticalLine.StrokeThickness = 0.1;
+                theVerticalLine.Stroke = Brushes.Black;
+                cnvsMap.Children.Add(theVerticalLine);
+            }
+
+            for (int i = 0; i < cnvsMap.Height / BASIC_UNIT; i++)
+            {
+                Line theHorizontal = new Line();
+                theHorizontal.X1 = 0;
+                theHorizontal.X2 = cnvsMap.Width;
+                theHorizontal.Y1 = i * BASIC_UNIT;
+                theHorizontal.Y2 = i * BASIC_UNIT;
+                theHorizontal.StrokeThickness = 0.1;
+                theHorizontal.Stroke = Brushes.Black;
+                cnvsMap.Children.Add(theHorizontal);
+            }
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cnvsMap.Width = Convert.ToInt32(txtLevelWidth.Text);
+            cnvsMap.Height = Convert.ToInt32(txtLevelHeight.Text);
+        }
+
+        private void cnvsMap_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(cnvsMap);
+            int x = (int)(p.X / BASIC_UNIT);
+            int y = (int)(p.Y / BASIC_UNIT);
+
+            Point p2 = new Point(x * BASIC_UNIT, y * BASIC_UNIT);
+
+
+            _clickStart = p2;
+            _clickCaptured = true;
+        }
+
+        private void cnvsMap_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_clickCaptured)
+            {
+                Point p = e.MouseDevice.GetPosition(cnvsMap);
+                int x = (int)(p.X / BASIC_UNIT);
+                int y = (int)(p.Y / BASIC_UNIT);
+
+                Point p2 = new Point(x * BASIC_UNIT, y * BASIC_UNIT);
+
+
+                _clickEnd = p2;
+
+                Rectangle r = new Rectangle();
+                r.Width = Math.Abs( _clickEnd.X - _clickStart.X);
+                r.Height = Math.Abs(_clickEnd.Y - _clickStart.Y);
+                r.Stroke = Brushes.Aqua;
+                r.StrokeThickness = .5;
+                r.Fill = Brushes.White;
+                cnvsMap.Children.Add(r);
+
+                cnvsMap.Children[cnvsMap.Children.Count - 1].SetValue(Canvas.LeftProperty, _clickStart.X);
+                cnvsMap.Children[cnvsMap.Children.Count - 1].SetValue(Canvas.TopProperty, _clickStart.Y);
+            }
+
+            _clickCaptured = false;
+        }
+
+        private void cnvsMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_clickCaptured)
+            {
+                if (cnvsMap.Children.Count > 0)
+                {
+                    if (cnvsMap.Children[cnvsMap.Children.Count - 1].GetType() == typeof(Rectangle))
+                        cnvsMap.Children.RemoveAt(cnvsMap.Children.Count - 1);
+                }
+
+
+                Point p = e.MouseDevice.GetPosition(cnvsMap);
+                int x = (int)(p.X / BASIC_UNIT);
+                int y = (int)(p.Y / BASIC_UNIT);
+
+                Point p2 = new Point(x * BASIC_UNIT, y * BASIC_UNIT);
+
+
+                _clickEnd = p2;
+
+                Rectangle r = new Rectangle();
+                r.Width = Math.Abs(_clickEnd.X - _clickStart.X);
+                r.Height = Math.Abs(_clickEnd.Y - _clickStart.Y);
+                r.Stroke = Brushes.Aqua;
+                r.StrokeThickness = .5;
+                r.Fill = Brushes.White;
+                cnvsMap.Children.Add(r);
+
+                double startX;
+                double startY;
+
+                if (_clickEnd.X < _clickStart.X)
+                {
+                    startX = (int)_clickEnd.X;
+                }
+                else
+                {
+                    startX = (int)_clickStart.X;
+                }
+
+                if (_clickEnd.Y < _clickStart.Y)
+                {
+                    startY = (int)_clickEnd.Y;
+                }
+                else
+                {
+                    startY = (int)_clickStart.Y;
+                }
+                cnvsMap.Children[cnvsMap.Children.Count - 1].SetValue(Canvas.LeftProperty, startX);
+                cnvsMap.Children[cnvsMap.Children.Count - 1].SetValue(Canvas.TopProperty, startY);
+            }
+        }
+    }
+}
