b5e0cbba37327b59a0d92432e60407dcb47d4369
sirpramalot <dwp11@zips.uakron.edu>
2013-08-25 21:34:13 -0400

    Fixing me blowing up Frank's changes

--- TeddysAdventure/TeddysAdventureLibrary/Teddy.cs
+++ TeddysAdventure/TeddysAdventureLibrary/Teddy.cs
@@ -368,9 +368,9 @@ namespace TeddysAdventureLibrary
                     {
                         Position = new Vector2(Position.X + overallVelocity.X, Position.Y);
 
-                        foreach (Rectangle surface in ((Screen)Game.Components[0]).Surfaces)
+                        foreach (Surface surface in ((Screen)Game.Components[0]).Surfaces)
                         {
-                            if (TeddyRectangle.Intersects(surface) )
+                            if (TeddyRectangle.Intersects(surface.Rect) )
                             {
                                 Position = new Vector2(surface.Right + 1, Position.Y);
                                 break;
@@ -383,9 +383,9 @@ namespace TeddysAdventureLibrary
                     //Move Screen
                     ((Screen)Game.Components[0]).MoveX((int)-overallVelocity.X);
 
-                    foreach (Rectangle surface in ((Screen)Game.Components[0]).Surfaces)
+                    foreach (Surface surface in ((Screen)Game.Components[0]).Surfaces)
                     {
-                        if (TeddyRectangle.Intersects(surface))
+                        if (TeddyRectangle.Intersects(surface.Rect))
                         {
                             ((Screen)Game.Components[0]).MoveX(-(surface.Right - (int)this.Position.X));
                             break;
@@ -418,9 +418,9 @@ namespace TeddysAdventureLibrary
                     {
                         Position = new Vector2(Position.X + overallVelocity.X, Position.Y);
 
-                        foreach (Rectangle surface in ((Screen)Game.Components[0]).Surfaces)
+                        foreach (Surface surface in ((Screen)Game.Components[0]).Surfaces)
                         {
-                            if (TeddyRectangle.Intersects(surface))
+                            if (TeddyRectangle.Intersects(surface.Rect))
                             {
                                 Position = new Vector2(surface.Left - (int)TeddyRectangle.Width  , Position.Y);
                                 break;
@@ -438,9 +438,9 @@ namespace TeddysAdventureLibrary
                 {
                     ((Screen)Game.Components[0]).MoveX((int)-overallVelocity.X);
 
-                    foreach (Rectangle surface in ((Screen)Game.Components[0]).Surfaces)
+                    foreach (Surface surface in ((Screen)Game.Components[0]).Surfaces)
                     {
-                        if (TeddyRectangle.Intersects(surface))
+                        if (TeddyRectangle.Intersects(surface.Rect))
                         {
                             ((Screen)Game.Components[0]).MoveX(-(surface.Left - (int)this.Position.X - (int)TeddyRectangle.Width));
                             break;
@@ -473,13 +473,13 @@ namespace TeddysAdventureLibrary
             }
 
 
-            foreach (Rectangle surfaceRect in ((Screen)Game.Components[0]).Surfaces)
+            foreach (Surface surfaceRect in ((Screen)Game.Components[0]).Surfaces)
             {
                 //Negative velocity is up (so teddy is jumping)
                 if (_yVelocity < 0)
                 {
                     //Check for surfaces above (Teddy hit his head)
-                    if (TeddyRectangle.Intersects(surfaceRect) & (TeddyRectangle.Top < surfaceRect.Bottom))
+                    if (TeddyRectangle.Intersects(surfaceRect.Rect) & (TeddyRectangle.Top < surfaceRect.Bottom))
                     {
                         //todo: if teddy is really close to one of the edge (only 1 pixel is touching the above surface, then move him over.  I'm pretty sure  Mario does that.
 
@@ -490,7 +490,7 @@ namespace TeddysAdventureLibrary
                 else
                 {
                     //Check for surfaces below
-                    if (TeddyRectangle.Intersects(surfaceRect) & (TeddyRectangle.Bottom > surfaceRect.Top))
+                    if (TeddyRectangle.Intersects(surfaceRect.Rect) & (TeddyRectangle.Bottom > surfaceRect.Top))
                     {
                         Position = new Vector2(Position.X, surfaceRect.Top - BoxToDraw.Height );
                         _yVelocity = 0.0f;
